ed114489c98a8d9f2d181df4fc67c99f9d489b5c
Lrd134 <larryc3200@gmail.com>
2023-02-05 05:13:25 -0500

    setup to handle data and ignoring multiple requests

--- frontend/src/LoadoutSelector.js
+++ frontend/src/LoadoutSelector.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 
 class LoadoutSelector extends Component {
-
-
+  constructor(props) {
+    this.state = {data: null, REQUESTING: false}
+  }
+  componentDidMount() {
+    getData();
+  }
   render() {
     return (
       <span className="LoadoutSelector">
