63b3e9e0e8e85fe5bd452be83343cb84518465ac
Lrd134 <larryc3200@gmail.com>
2023-02-05 02:39:46 -0500

    this component will handle everything

---
+++ src/LoadoutSelector.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LoadoutSelector extends Component {
+  return (
+    <span className="LoadoutSelector">
+      <div className="items">
+      
+      </div>
+    </span>
+  )
+}
+
+return LoadoutSelector;
--- src/PhasmophobiaLoadoutCalculator.js
+++ src/PhasmophobiaLoadoutCalculator.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoadoutSelector from './LoadoutSelector';
 
 function PhasmophobiaLoadoutCalculator() {
   return (
     <div className="PhasmophobiaLoadoutCalculator">
-
+      <LoadoutSelector />
     </div>
   );
 }
