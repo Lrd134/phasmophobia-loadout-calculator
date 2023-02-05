7c2d3ba0920865f0c8f01f9441425ba7840f7799
Lrd134 <larryc3200@gmail.com>
2023-02-05 03:15:27 -0500

    finished items

--- backend/items.js
+++ backend/items.js
@@ -12,7 +12,7 @@ const items = {
   emf: {
     price: 45,
     max: 2,
-    photo: "EMFReader.png"
+    photo: "EMF_Reader.png"
   },
   flashlight: {
     price: 30,
@@ -22,22 +22,91 @@ const items = {
   ghostBook: {
     price: 40,
     max: 2,
-    photo: "ClosedBook.png"
+    photo: "Closed_Book.png"
   },
   spiritBox: {
     price: 50,
     max: 2,
-    photo: "SpiritBox.png"
+    photo: "Spirit_Box.png"
   },
   uv: {
     price: 35,
     max: 2,
-    photo: "UVLight.png"
+    photo: "UV_Light.png"
   },
   camera: {
     price: 40,
     max: 3,
     photo: "Camera.png"
   },
-  
+  candle: {
+    price: 15,
+    max: 4,
+    photo: "Candle.png"
+  },
+  crucifix: {
+    price: 30,
+    max: 2,
+    photo: "Crucifix.png"
+  },
+  glowstick: {
+    price: 20,
+    max: 2,
+    photo: "Glowstick.png"
+  },
+  headCamera: {
+    price: 60,
+    max: 4,
+    photo: "Head_Camera.png"
+  },
+  lighter: {
+    price: 10,
+    max: 4,
+    photo: "Lighter.png"
+  },
+  motionSensor: {
+    price: 100,
+    max: 4,
+    photo: "Motion_Sensor.png"
+  },
+  parabolicMicrophone: {
+    price: 50,
+    max: 2,
+    photo: "Parabolic.png"
+  },
+  salt: {
+    price: 15,
+    max: 3,
+    photo: "Salt.png"
+  },
+  sanityPills: {
+    price: 20,
+    max: 4,
+    photo: "Sanity_Pills.png"
+  },
+  smudge: {
+    price: 15,
+    max: 4,
+    photo: "Smudge_Sticks.png"
+  },
+  soundSensor: {
+    price: 80,
+    max: 4,
+    photo: "Sound_Sensor.png"
+  },
+  strongFlashlight: {
+    price: 50,
+    max: 4,
+    photo: "Strong_Flashlight.png"
+  },
+  thermometor: {
+    price: 30,
+    max: 3,
+    photo: "Thermometor.png" 
+  },
+  tripod: {
+    price: 25,
+    max: 5,
+    photo: "Tripod.png"
+  }
 }
