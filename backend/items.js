61fa74de22460d13a7ee4cce0190eeb25d17fc2a
Lrd134 <larryc3200@gmail.com>
2023-02-05 03:05:01 -0500

    adding item data

---
+++ backend/items.js
@@ -0,0 +1,43 @@
+const items = {
+  videoCamera: {
+    price: 50,
+    max: 6,
+    photo: "Video_Camera.png"
+  }
+  dots: {
+    price: 65,
+    max: 2,
+    photo: "DOTS_Projector.png"
+  },
+  emf: {
+    price: 45,
+    max: 2,
+    photo: "EMFReader.png"
+  },
+  flashlight: {
+    price: 30,
+    max: 4,
+    photo: "Flashlight.png"
+  },
+  ghostBook: {
+    price: 40,
+    max: 2,
+    photo: "ClosedBook.png"
+  },
+  spiritBox: {
+    price: 50,
+    max: 2,
+    photo: "SpiritBox.png"
+  },
+  uv: {
+    price: 35,
+    max: 2,
+    photo: "UVLight.png"
+  },
+  camera: {
+    price: 40,
+    max: 3,
+    photo: "Camera.png"
+  },
+  
+}
