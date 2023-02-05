f7bdff0025d45167b37987ec771b3a7aa6f62be0
Lrd134 <larryc3200@gmail.com>
2023-02-05 06:11:55 -0500

    fixed from typescript to js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemReducer from './itemSlice.js'
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     items: itemReducer,
   }
 })
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
